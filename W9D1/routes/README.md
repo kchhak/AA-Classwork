21f118825baf557fea368615774e56f20c0f5135
Kaneka <kane.chhak@gmail.com>
2019-08-26 16:45:23 -0700

    Complete First Routes and Controllers project

---
+++ W9D1/routes/README.md
@@ -0,0 +1,6 @@
+rails new routes_project -G--database=postgresql
+
+
+User.new(name: 'Justin', email: 'justin@gmail.com')
+
+User.new(name: 'Kevin', email: 'kevin@gmail.com')
---
+++ W9D1/routes/routes_project/app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :text             not null
+#  email      :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :name, :email, presence: true
+
+end
